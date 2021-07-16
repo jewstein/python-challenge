82b942a112e9d3de21bc9c61cbc79cd7bb86b8f8
Ashley Vetters <ashleyvetters@Ashleys-MacBook-Pro.local>
2021-07-16 13:27:02 -0500

    first lines of code

---
+++ PyBank/main.py
@@ -0,0 +1,2 @@
+import os, import csv
+csvpath = os.path.join('..', 'Resources','budget_data.csv')
---
+++ PyPoll/main.py
@@ -0,0 +1,2 @@
+import os, import csv
+csvpath = os.path.join('..', 'Resources','election_data.csv')
